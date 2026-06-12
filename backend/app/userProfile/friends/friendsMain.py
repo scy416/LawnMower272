5e8e09eafdbf3c9fa989614403b08ee6aa9ee7dd
Shu Changyu <changyu416@gmail.com>
2026-06-13 00:46:40 +0800

    Feature: Initial draft of friends system

--- backend/app/userProfile/friends/friendsMain.py
+++ backend/app/userProfile/friends/friendsMain.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
 from app.database.database import get_db
 from app.auth.dependencies import get_current_user
@@ -9,9 +10,7 @@ router = APIRouter(prefix="/friends", tags =["friends"])
 
 @router.get("/friends_list", response_model = List[FriendResponse], status_code=status.HTTP_200_OK)
 def get_friends(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    friends = current_user.profile.friends
+    friend_profiles = current_user.profile.friends
+    friends = [profile.user for profile in friend_profiles]
     return friends
 
-@router.post("/add/{user_id}",)
-def add_friend(friend: User, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    return 
--- backend/app/userProfile/friends/requesthandler.py
+++ backend/app/userProfile/friends/requesthandler.py
@@ -4,26 +4,34 @@ from sqlalchemy.orm import Session
 from app.database.database import get_db
 from app.auth.dependencies import get_current_user
 from app.database.models import User, UserProfile, FriendRequest
+from app.userProfile.upExceptions import sendRequestExceptions, acceptRequestExceptions
 
 router = APIRouter(prefix="/friends/requests", tags=["requests"])
 
-@router.post("/send")
+@router.post("/send/{receiver_id}")
 def send_friend_request(receiver_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     sender_id = current_user.id
-    #exceptions to handle
-    # 1. Prevent sending a request to yourself
-    # 2. Check if they are already friends
-    # 3. Check if a request is already pending (either direction)
-    new_friend_request = FriendRequest(sender_id, receiver_id, "pending")
+
+    sendRequestExceptions(receiver_id, current_user, db)
+
+    new_friend_request = FriendRequest(sender_id = sender_id, receiver_id = receiver_id, status = "pending")
     db.add(new_friend_request)
     db.commit()
 
     return {"message": "Friend request sent!"}
 
-@router.post("/accept")
+@router.post("/accept/{request_id}")
 def accept_friend_request(request_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    friend_request = db.query(FriendRequest).filter(FriendRequest.id == request_id).findFirst()
-    sender_profile = db.query(UserProfile).filter(UserProfile.user.id == friend_request.sender_id).findFirst()
+    friend_request = db.query(FriendRequest).filter(FriendRequest.id == request_id).first()
+    
+    acceptRequestExceptions(friend_request, current_user)
 
-    sender_profile.append(current_user.profile)
-    return 
+    sender_profile = db.query(UserProfile).filter(UserProfile.user.id == friend_request.sender_id).first()
+    receiver_profile = current_user.profile
+
+    sender_profile.friends.append(receiver_profile)
+    receiver_profile.friends.append(sender_profile)
+    db.delete(friend_request)
+    db.commit()
+
+    return {"message": "Friend request accepted!"}
