5add15ee07c7f0b6f93f490fde56a7e3fa8b4538
Shu Changyu <changyu416@gmail.com>
2026-07-23 22:04:08 +0800

    fixed CORs issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://lawn-mower272.vercel.app",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
