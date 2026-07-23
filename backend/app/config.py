f6d24f51353985a4f65b069124c3e8c406bd859a
Shu Changyu <changyu416@gmail.com>
2026-07-23 20:17:03 +0800

    deployment 3

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str 
-    DATABASE_URL: str
+    SECRET_KEY: str = "default"
+    DATABASE_URL: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  
     ALGORITHM: str = "HS256"
     GEMINI_API_KEY: str | None = None
