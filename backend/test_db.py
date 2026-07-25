a296bae9dc56e47739c40039cf458bc2845b427f
Shu Changyu <changyu416@gmail.com>
2026-07-25 10:45:04 +0800

    Re: Organized files, deleted unnecessary files

--- backend/test_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('C:/Users/User/orbital/LawnMower272/backend/test.db')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM user_profiles")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
